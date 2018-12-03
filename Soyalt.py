d81b998afc5b08e99e89b2e767a8a5e5d445409f
soyalk1 <45288561+soyalk1@users.noreply.github.com>
2018-12-03 14:55:00 +0530

    Update Soyalt.py

--- Soyalt.py
+++ Soyalt.py
@@ -50,7 +50,7 @@ async def botinvite(ctx):
 
 
 @client.command(pass_context=True)  
-@commands.has_permissions(kick_members=True)     
+@commands.check(is_owner)     
 async def kick(ctx,user:discord.Member):
     if user.server_permissions.kick_members:
       await client.say('**He is mod/admin and i am unable to kick him/her**')
