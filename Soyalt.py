b7a6f13a270a81666c396bf14e61335536e20c80
soyalk1 <45288561+soyalk1@users.noreply.github.com>
2018-12-04 15:07:08 +0530

    Update Soyalt.py

--- Soyalt.py
+++ Soyalt.py
@@ -466,39 +466,4 @@ async def clear(ctx, number):
     await client.delete_messages(mgs)      
 
 
-@client.command(pass_context = True)
-@commands.has_permissions(administrator=True)
-async def setuppartner(ctx):
-    if ctx.message.author.bot:
-      return
-    else:
-      server = ctx.message.server
-      everyone_perms = discord.PermissionOverwrite(send_messages=False, read_messages=True)
-      everyone = discord.ChannelPermissions(target=server.default_role, overwrite=everyone_perms)
-      await client.create_channel(server, '★-Soyal-partner-★',everyone)
-      message = "░▒▓█►─═ Soyal offical ═─◄█▓▒░ \n Our server link: https://discord.gg/xJR6Bs9 \n Here's a friendly community where you can enjoy. Here people are active . It's amazing also we have so many bots like- tatsumaki, pokecord, unbeliva boat,etc . \n Features: \n =>Verification \n =>Get answers of any types of questions \n => Active Staffs and Support =>Diverse server =>------------------------------"
-      channel = ctx.message.server.channel.name == "★-Soyal-partner-★"
-      await client.send_message(channel, embed=embed) 
-
-	
-@client.command(pass_context=True)
-@commands.cooldown(rate=1,per=86400,type=BucketType.user) 
-@commands.has_permissions(administrator=True)
-async def partner(ctx, *, msg=None):
-    channel = discord.utils.get(client.get_all_channels(), name='★-Soyal-partner-★')
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    if ctx.message.server.id == '483216930506014723':
-      return
-    if not msg:
-      await client.say('Please specify a partnership description to post')
-    else:
-      embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-      embed.add_field(name='Discord Partner', value='-------------------',inline = False) 
-      embed.add_field(name='Partner ID:', value='{}'.format(ctx.message.author.id),inline = False)
-      embed.add_field(name='Partner Name:', value='{}'.format(ctx.message.author.name),inline = False)
-      embed.add_field(name='Server Name:', value='{}'.format(ctx.message.server.name),inline = False)
-      embed.add_field(name='Partnership Description:', value=msg, inline=False)
-      await client.send_message(channel, embed=embed) 
-      await client.delete_message(ctx.message)
-
 client.run(os.getenv('Token'))
