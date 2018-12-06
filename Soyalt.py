b33b5f447ef56d0d247d8db2ea7f13a329e88644
soyalk1 <45288561+soyalk1@users.noreply.github.com>
2018-12-06 21:13:38 +0530

    Update Soyalt.py

--- Soyalt.py
+++ Soyalt.py
@@ -10,18 +10,29 @@ import os
 import functools
 import time
 import datetime
+Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
+client = Bot(description="Here is some commands for you", command_prefix="s!", pm_help = True)
 
-client = commands.Bot(description="Here is some command for you", command_prefix=commands.when_mentioned_or("s!"), pm_help = False)
 
+async def status_task():
+    while True:
+        await client.change_presence(game=discord.Game(name='for Soyal || s!help'))
+        await asyncio.sleep(5)
+        await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
+        await asyncio.sleep(5)
+        await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
+        await asyncio.sleep(5)
+        
 
-
+	
 @client.event
 async def on_ready():
-	print('Logged in as '+client.user.name+'')
-	print('--------')
-	print('--------')
-	print('Started Soyal') #add_your_bot_name_here
-	return await client.change_presence(game=discord.Game(name='Soyal || s!help ')) #add_your_bot_status_here
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('--------')
+    print('Started Dark BOT')
+    print('Created by Utkarsh')
+    client.loop.create_task(status_task())
 	
 def is_owner(ctx):
     return ctx.message.author.id == "472680171451973632,485868646854557696" #replace_it_with_your_discord_id
